e96babbe0bd841e1b67cdb63c53dd1bc914005f5
Morozov Artem <artemmorozov155@gmail.com>
2025-02-20 15:44:09 +0300

    Added a list that contains all the measurements

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from Converters.Converter_ad5t_To_xlsx import ad5t_TO_xlsx
 from Extract_data import Data_create
 from Create_Excel import Create_excel
 
+def Generation_BigDate(Data1):
+    Biiig_Data_list = []
+    for key in Data1:
+        for i in range(len(Data1[key])):
+            Biiig_Data_list.append(Data1[key][i])
+    return Biiig_Data_list
 
 folder_1_name = "PM"
 folder_2_name = "PMinDie"
@@ -16,6 +22,7 @@ while True:
 
     # -------Извлекаем необходимые данные из файлов и переносим их в новый xlsx файл
     Data = Data_create(root_directory, folder_1_name, folder_2_name, name)
+    Biiig_Data = Generation_BigDate(Data)  # Список со всеми значениями
     Create_excel(Data, name, root_directory)
 
 
