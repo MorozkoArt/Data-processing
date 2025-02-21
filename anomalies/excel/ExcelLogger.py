4cef0471af0e6eaef8e4e887bb07fbe415c7be32
Sergho <sergche04@gmail.com>
2025-02-21 11:14:21 +0300

    fix: conditional number format

--- anomalies/excel/ExcelLogger.py
+++ anomalies/excel/ExcelLogger.py
@@ -1,5 +1,5 @@
 from openpyxl.styles import Alignment, PatternFill, Font
-from common.constants import ANOMALIES_BETA, ANOMALIES_BLOCKS_OFFSETS, ANOMALIES_COMPARABLE_FUNC, ANOMALIES_DATA_HEADINGS, ANOMALIES_DATA_VALUES, ANOMALIES_ERROR_BACKGROUND_COLOR, ANOMALIES_ERROR_TEXT_COLOR, ANOMALIES_NUMBER_FORMAT, ANOMALIES_PARAMS_HEADINGS, ANOMALIES_PARAMS_VALUES
+from common.constants import ANOMALIES_BETA, ANOMALIES_BLOCKS_OFFSETS, ANOMALIES_COMPARABLE_FUNC, ANOMALIES_DATA_HEADINGS, ANOMALIES_DATA_VALUES, ANOMALIES_ERROR_BACKGROUND_COLOR, ANOMALIES_ERROR_TEXT_COLOR, ANOMALIES_NORMAL_NUMBER_FORMAT, ANOMALIES_NUMBER_FORMAT_CONDITIONS, ANOMALIES_PARAMS_HEADINGS, ANOMALIES_PARAMS_VALUES, ANOMALIES_STRICT_NUMBER_FORMAT
 
 class ExcelLogger:
     def __init__(self, column, sheet, offset):
@@ -17,7 +17,10 @@ class ExcelLogger:
         for index, function in enumerate(ANOMALIES_PARAMS_VALUES):
             value = getattr(self.column, function)()
             cell = self.sheet.cell(row=2+self.offset["row"]+offset["row"],column=1+index+self.offset["col"]+offset["col"], value=value)
-            cell.number_format = ANOMALIES_NUMBER_FORMAT
+            if ANOMALIES_NUMBER_FORMAT_CONDITIONS["from"] <= abs(cell.value) <= ANOMALIES_NUMBER_FORMAT_CONDITIONS["to"]:
+                cell.number_format = ANOMALIES_NORMAL_NUMBER_FORMAT
+            else:
+                cell.number_format = ANOMALIES_STRICT_NUMBER_FORMAT
             cell.alignment = Alignment(horizontal="center", vertical="center")
     def logData(self):
         offset = ANOMALIES_BLOCKS_OFFSETS[type(self.column).__name__]["data"]
@@ -28,7 +31,10 @@ class ExcelLogger:
             list = getattr(self.column, function)()
             for valueIndex, value in enumerate(list):
                 cell = self.sheet.cell(row=2+valueIndex+self.offset["row"]+offset["row"],column=1+functionIndex+self.offset["col"]+offset["col"], value=value)
-                cell.number_format = ANOMALIES_NUMBER_FORMAT
+                if ANOMALIES_NUMBER_FORMAT_CONDITIONS["from"] <= abs(cell.value) <= ANOMALIES_NUMBER_FORMAT_CONDITIONS["to"]:
+                    cell.number_format = ANOMALIES_NORMAL_NUMBER_FORMAT
+                else:
+                    cell.number_format = ANOMALIES_STRICT_NUMBER_FORMAT
                 cell.alignment = Alignment(horizontal="center", vertical="center")
                 if function == ANOMALIES_COMPARABLE_FUNC and value > ANOMALIES_BETA:
                     cell.fill = PatternFill(start_color=ANOMALIES_ERROR_BACKGROUND_COLOR, fill_type = "solid")
